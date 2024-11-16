f916008065482d47132af9397ae417202c62a1e6
sxavenger <sxavenger.x3@gmail.com>
2024-11-16 18:06:42 +0900

    a

--- project/Engine/System/Runtime/Scene/GameScene.cpp
+++ project/Engine/System/Runtime/Scene/GameScene.cpp
@@ -54,6 +54,7 @@ void GameScene::Init() {
 	mainWindow_ = SxavengerSystem::CreateMainWindow(kMainWindowSize, kMainWindowTitle);
 	mainWindow_->SetIcon("resources/icon/SxavengerEngineIcon.ico", { 32, 32 });
 
+
 }
 
 void GameScene::Update() {
