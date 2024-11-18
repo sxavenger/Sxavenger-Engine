2b852e2bd789fddea71b9fcaf8befc0993bb7cf7
sxavenger <sxavenger.x3@gmail.com>
2024-11-18 17:16:22 +0900

    Directoryの追加

--- project/Engine/System/DirectX/DxObject/DxCompileBlobCollection.cpp
+++ project/Engine/System/DirectX/DxObject/DxCompileBlobCollection.cpp
@@ -1,6 +1,12 @@
 #include "DxCompileBlobCollection.h"
 _DXOBJECT_USING
 
+//-----------------------------------------------------------------------------------------
+// include
+//-----------------------------------------------------------------------------------------
+//* engine
+#include <Engine/System/Config/SxavengerDirectory.h>
+
 ////////////////////////////////////////////////////////////////////////////////////////////
 // CompileBlobCollection class methods
 ////////////////////////////////////////////////////////////////////////////////////////////
@@ -15,7 +21,8 @@ void CompileBlobCollection::Term() {
 std::weak_ptr<ComPtr<IDxcBlob>> CompileBlobCollection::TryCreateBlob(const std::wstring& filename, CompileProfile profile) {
 
 	if (!blobs_.Contains(filename)) {
-		std::shared_ptr<ComPtr<IDxcBlob>> blob = std::make_shared<ComPtr<IDxcBlob>>(compiler_->Compile(filename, profile, L"main"));
+		std::shared_ptr<ComPtr<IDxcBlob>> blob
+			= std::make_shared<ComPtr<IDxcBlob>>(compiler_->Compile(kShaderDirectory + filename, profile, L"main"));
 
 		blobs_.Emplace(filename, { std::move(blob), profile});
 	}
@@ -28,7 +35,7 @@ void CompileBlobCollection::HotReload(const std::wstring& filename) {
 		return; //!< filenameのblobが見つからなかったため
 	}
 
-	blobs_[filename].blob = std::make_shared<ComPtr<IDxcBlob>>(compiler_->Compile(filename, blobs_.At(filename).profile, L"main"));
+	blobs_[filename].blob = std::make_shared<ComPtr<IDxcBlob>>(compiler_->Compile(kShaderDirectory + filename, blobs_.At(filename).profile, L"main"));
 }
 
 std::weak_ptr<ComPtr<IDxcBlob>> CompileBlobCollection::GetBlob(const std::wstring& filename) {
