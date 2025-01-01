80021891a792ca1bba8ff2407f34abb0bef31c53
sxavenger <sxavenger.x3@gmail.com>
2025-01-01 16:11:59 +0900

    SceneDepthの追加

---
+++ project/Engine/!Render/FSceneDepth.h
@@ -0,0 +1,89 @@
+#pragma once
+
+//-----------------------------------------------------------------------------------------
+// include
+//-----------------------------------------------------------------------------------------
+//* engine
+#include <Engine/System/Utility/ComPtr.h>
+#include <Engine/System/DirectX/DxObject/DxObjectCommon.h>
+#include <Engine/System/DirectX/DxObject/DxDescriptor.h>
+#include <Engine/System/DirectX/DirectXContext.h>
+
+//* lib
+#include <Lib/Geometry/Vector2.h>
+
+////////////////////////////////////////////////////////////////////////////////////////////
+// Framework SceneDepth class
+////////////////////////////////////////////////////////////////////////////////////////////
+class FSceneDepth { 
+public:
+
+	//=========================================================================================
+	// public methods
+	//=========================================================================================
+
+	FSceneDepth()  = default;
+	~FSceneDepth() { Term(); }
+
+	void Create(const Vector2ui& size);
+
+	void Term();
+
+private:
+
+	////////////////////////////////////////////////////////////////////////////////////////////
+	// RasterizerDepth structure
+	////////////////////////////////////////////////////////////////////////////////////////////
+	struct RasterizerDepth {
+
+		//* member *//
+
+		ComPtr<ID3D12Resource> resource_;
+		DxObject::Descriptor descriptorDSV_;
+		DxObject::Descriptor descriptorSRV_;
+
+		//* method *//
+
+		void Create(const Vector2ui& size); //!< Format is default depth format only.
+
+		void Delete();
+
+		void BeginWrite(const DirectXThreadContext* context) const; //!< Transition to depth write state.
+
+		void EndWrite(const DirectXThreadContext* context) const; //!< Transition to pixel shader resource state.
+
+		void ClearDepth(const DirectXThreadContext* context) const; //!< Clear depth value.
+
+	};
+
+	////////////////////////////////////////////////////////////////////////////////////////////
+	// RaytracingDepth structure
+	////////////////////////////////////////////////////////////////////////////////////////////
+	struct RaytracingDepth {
+
+		//* member *//
+
+		ComPtr<ID3D12Resource> resource_;
+		DxObject::Descriptor descriptorUAV_;
+
+		//* method *//
+
+		void Create(const Vector2ui& size); //!< Format is default depth compatible format only.
+
+		void Delete();
+
+		void BeginWrite(const DirectXThreadContext* context) const; //!< Transition to unordered access state.
+
+		void EndWrite(const DirectXThreadContext* context) const; //!< Transition to pixel shader resource state.
+
+	};
+
+	//=========================================================================================
+	// private variables
+	//=========================================================================================
+
+	RasterizerDepth rasterizer_;
+	RaytracingDepth raytracing_;
+
+	// todo: 深度プリパスの追加
+};
---
+++ project/Engine/!Render/FSceneTextures.cpp
@@ -0,0 +1,1 @@
+#include "FSceneTextures.h"
