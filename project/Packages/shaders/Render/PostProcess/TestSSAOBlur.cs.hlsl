0979739739fed9fd717fabb3ee9b00cc4d684743
sxavenger <sxavenger.x3@gmail.com>
2025-02-02 16:57:42 +0900

    closed

--- project/Packages/shaders/Render/PostProcess/TestSSAOBlur.cs.hlsl
+++ project/Packages/shaders/Render/PostProcess/TestSSAOBlur.cs.hlsl
@@ -8,9 +8,9 @@
 // buffers
 //=========================================================================================
 
-RWTexture2D<float4> gInput        : register(u0); //!< input texture
+RWTexture2D<float4> gInput : register(u0); //!< input texture
 RWTexture2D<float4> gIntermediate : register(u1); //!< intermediate texture
-RWTexture2D<float4> gOutput       : register(u2); //!< output texture
+RWTexture2D<float4> gOutput : register(u2); //!< output texture
 
 SamplerState gSampler : register(s0);
 
@@ -44,6 +44,35 @@ float Gaussian2d(float2 diff, float sigma) {
 	return a * b;
 }
 
+float CrossBilateralWeight(float r, float ddiff, inout float weightTotal) {
+	float w = exp(-r * r * 2.0f * pi_v) * (ddiff < gParameter.filter ? 1.0 : 0.0);
+	weightTotal += w;
+	return w;
+}
+
+// Perform a gaussian blur in one direction
+float Blur(float2 texcoord) {
+	float weightTotal = 1.0;
+	float aoDepth = gIntermediate[texcoord].r;
+	float totalAO = aoDepth.x;
+	float centerZ = gPosition[texcoord].z;
+	
+	static const int kKernelRadius = 15;
+	// [unroll]
+	for (int i = -kKernelRadius; i < kKernelRadius; i++) {
+		float2 current = texcoord + (float(i) * 2.0f);
+		
+		float sampleAO = gIntermediate[current].r;
+		float sampleZ = gPosition[current].z;
+		
+		float diff   = abs(sampleZ - centerZ);
+		float weight = CrossBilateralWeight(float(i), diff, weightTotal);
+		totalAO     += sampleAO * weight;
+	}
+
+	return totalAO / weightTotal;
+}
+
 ////////////////////////////////////////////////////////////////////////////////////////////
 // main
 ////////////////////////////////////////////////////////////////////////////////////////////
@@ -56,38 +85,13 @@ void main(uint3 dispatchThreadId : SV_DispatchThreadID) {
 	if (any(index >= size)) {
 		return; //!< texture size over
 	}
-
 	
-	//float4 ao = gIntermediate[index];
-	//gOutput[index] = float4(ao.rgb, 1.0f);
-	//return;
+	float2 texcoord = float2(index) / float2(size);
 	
-	static const int2 kStrength = int2(gParameter.scale);
-	
-	float sum = 0.0f;
-	float weightSum = 0.0f;
-	
-	for (int x = -kStrength.x; x < kStrength.x; ++x) {
-		for (int y = -kStrength.x; y < kStrength.y; ++y) {
-			
-			int2 i = int2(index) + int2(x, y);
-
-			float ao     = gIntermediate[i].r;
-			float weight = Gaussian2d(float2(x, y), 24.0f);
-			
-			sum       += ao * weight;
-			weightSum += weight;
-			
-		}
-	}
-	
-	float ambient = saturate(sum / weightSum);
+	float blur = Blur(texcoord);
 	
 	float4 input = gInput[index];
 	
-	gOutput[index] = float4(input.rgb * ambient, input.a);
-	
-	//float ao = gIntermediate[index].r;
-	//gOutput[index] = float4(ao, ao, ao, 1.0f);
+	gOutput[index] = float4(input.rgb * blur, input.a);
 	
 }
