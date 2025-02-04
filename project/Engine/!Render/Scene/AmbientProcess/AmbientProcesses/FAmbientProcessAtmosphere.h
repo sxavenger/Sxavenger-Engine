b4a47a09fea7bf7f3c86008f6118067a69020b9b
sxavenger <sxavenger.x3@gmail.com>
2025-02-04 23:20:43 +0900

    fixed atmosphere

--- project/Engine/!Render/Scene/AmbientProcess/AmbientProcesses/FAmbientProcessAtmosphere.h
+++ project/Engine/!Render/Scene/AmbientProcess/AmbientProcesses/FAmbientProcessAtmosphere.h
@@ -27,28 +27,12 @@ public:
 	// Prameter structure
 	////////////////////////////////////////////////////////////////////////////////////////////
 	struct Parameter {
-		Color4f waveLength; //!< 波長
-		Vector3f lightDir;
-		float innerRadius;  //!< 内側の（惑星の）半径
-		float outerRadius;  //!< 外側の（大気の）半径
-		float kr;           //!< レイリー散乱係数
-		float km;           //!< ミー散乱係数
-		float eSun;         //!< 太陽の強さ
-		float g;            //!< ミー散乱の位相関数のパラメーター
-		float scaleDepth;  //!< 大気の平均密度が見つかる高度
-		float exposure;
+		Vector3f direction;
+		float    intensity;
 
-		void Reset() {
-			lightDir    = {0.0f, 1.0f, 0.0f};
-			waveLength  = { 0.68f, 0.55f, 0.44f, 1.0f };
-			innerRadius = 8.0f;
-			outerRadius = innerRadius + 16.0f;
-			kr          = 0.0025f;
-			km          = 0.001f;
-			eSun        = 128.0f;
-			g           = -0.95f;
-			scaleDepth  = 0.25f;
-			exposure    = 0.8f;
+		void Init() {
+			direction = { 0.0f, 1.0f, 0.0f };
+			intensity = 20.0f;
 		}
 	};
 
--- project/Packages/shaders/Render/AmbientProcess/Atmosphere.cs.hlsl
+++ project/Packages/shaders/Render/AmbientProcess/Atmosphere.cs.hlsl
@@ -11,41 +11,39 @@
 RWTexture2D<float4> gMain : register(u0); //!< main texture
 
 struct Parameter {
-	float4 waveLength;
-	float3 lightDir;
-	float innerRadius;
-	float outerRadius;
-	float kr;
-	float km;
-	float eSun;
-	float g;
-	float scaleDepth;
-	float exposure;
+	float3 direction;
+	float intensity;
 };
 ConstantBuffer<Parameter> gParameter : register(b0);
 
 ConstantBuffer<Camera> gCamera : register(b1);
 
-//=========================================================================================
-// static variables
-//=========================================================================================
+////////////////////////////////////////////////////////////////////////////////////////////
+// static const variables
+////////////////////////////////////////////////////////////////////////////////////////////
 
-static const float pi_v = 3.1415926535897932384626433832795f;
+static const float pi = 3.1415926535897932384626433832795f;
 
-static const float3 kInvWavelength = 1.0f / pow(gParameter.waveLength.rgb, 4.0f);
+////////////////////////////////////////////////////////////////////////////////////////////
+// static const parameter variables
+////////////////////////////////////////////////////////////////////////////////////////////
 
-static const float kInnerRadiusPow2 = pow(gParameter.innerRadius, 2.0f);
-static const float kOuterRadiusPow2 = pow(gParameter.outerRadius, 2.0f);
+//static const float3 kSunDir      = normalize(float3(0.0f, 1.0f, 10.0f)); //!< 太陽の方向
+//static const float kSunIntensity = 20.0f;                               //!< 太陽の強さ
 
-static const float kScale               = 1.0f / (gParameter.outerRadius - gParameter.innerRadius);
-static const float kScaleOverScaleDepth = kScale / gParameter.scaleDepth;
+static const uint kNumSamples = 16; //!< サンプリング数
+static const uint kNumScatter = 8; //!< 散乱回数
 
-static const float kKrESun = gParameter.kr * gParameter.eSun;
-static const float kKmESun = gParameter.km * gParameter.eSun;
-static const float kKr4pi  = gParameter.kr * pi_v * 4.0f;
-static const float kKm4pi  = gParameter.km * pi_v * 4.0f;
+static const float kEarthRadius      = 6360e3; //!< 地球の半径
+static const float kAtmosphereRadius = 6420e3; //!< 大気の半径
 
-static const float kG2 = gParameter.g * gParameter.g;
+static const float kG = 0.76f; //!< Henyey-Greensteinのg係数
+
+static const float kHr = 7994.0f; //!< Rayleighのスケールハイト
+static const float kHm = 1200.0f; //!< Mieのスケールハイト
+
+static const float3 kBetaR = float3(5.8e-6, 13.5e-6, 33.1e-6); //!< Rayleighの散乱係数
+static const float3 kBetaM = float3(21e-6, 21e-6, 21e-6);      //!< Mieの散乱係数
 
 ////////////////////////////////////////////////////////////////////////////////////////////
 // structures
@@ -60,7 +58,6 @@ struct Sphere {
 	float3 center;
 	float radius;
 };
-
 
 ////////////////////////////////////////////////////////////////////////////////////////////
 // methods
@@ -78,29 +75,124 @@ Ray GenerateRay(float2 uv) {
 	Ray ray = (Ray)0;
 	
 	// カメラ空間でのrayをワールド空間に変換
-	ray.direction = normalize(mul(float4(view.xyz, 0.0f), gCamera.world).xyz);
+	ray.direction = normalize(mul(float4(view.xyz, 0.0f), gCamera.world).xyz); 
 	ray.origin    = gCamera.GetPosition();
 	
 	return ray;
 }
 
-float3 IntersectionPos(float3 rayPos, float3 rayDir, float sphereRadius) {
-	const float A = dot(rayDir, rayDir);
-	const float B = 2.0 * dot(rayPos, rayDir);
-	const float C = dot(rayPos, rayPos) - sphereRadius * sphereRadius;
-
-	float discriminant = B * B - 4.0 * A * C;
-	if (discriminant < 0.0f)
-		return float3(0, 0, 0); // 交差なし
-
-	float t = (-B - sqrt(discriminant)) / (2.0 * A);
-	return rayPos + rayDir * t;
+float RayleighPhaseFunction(float mu) {
+	return 3.0f / (16.0f * pi) * (1.0f + mu * mu);
 }
 
-float IntegralApproximation(float fCos) {
-	float x = 1.0 - fCos;
-	return gParameter.scaleDepth * exp(-0.00287 + x * (0.459 + x * (3.83 + x * (-6.80 + x * 5.25))));
+float HenyeyGreensteinPhaseFunction(float mu) {
+	return 1.0f / (4.0f * pi) * (1.0f - kG * kG) / pow(1.0f + kG * kG - 2.0f * kG * mu, 1.5f);
 }
+
+bool IntersectSphere(Ray ray, Sphere sphere, out float distanceNear, out float distanceFar) {
+	float3 rc     = sphere.center - ray.origin;
+	float radius2 = sphere.radius * sphere.radius;
+	float tca     = dot(rc, ray.direction);
+	float d2      = dot(rc, rc) - tca * tca;
+	
+	if (d2 > radius2) {
+		return false;
+	}
+	
+	float thc = sqrt(radius2 - d2);
+	
+	distanceNear = tca - thc;
+	distanceFar  = tca + thc;
+	return true;
+}
+
+bool GetSunLight(Ray ray, Sphere sphere, out float opticalDepthR, out float opticalDepthM) {
+	
+	float distanceNear = 0.0f;
+	float distanceFar  = 0.0f;
+	
+	if (!IntersectSphere(ray, sphere, distanceNear, distanceFar)) {
+		return false;
+	}
+	
+	float marchStep = distanceFar / kNumSamples;
+	
+	opticalDepthR = 0.0f;
+	opticalDepthM = 0.0f;
+	
+	for (uint i = 0; i < kNumSamples; ++i) {
+		
+		float3 samplePoint = ray.origin + ray.direction * ((marchStep * i) + 0.5f * marchStep);
+		float height = length(samplePoint) - kEarthRadius;
+		
+		if (height < 0.0f) {
+			return false; //!< 地表に到達
+		}
+		
+		opticalDepthR += exp(-height / kHr) * marchStep;
+		opticalDepthM += exp(-height / kHm) * marchStep;
+	}
+	
+	return true;
+}
+
+float3 GetIncidentLight(Ray ray, Sphere atmosphere) {
+	
+	float distanceNear = 0.0f;
+	float distanceFar  = 0.0f;
+
+	if (!IntersectSphere(ray, atmosphere, distanceNear, distanceFar)) {
+		return float3(1.0f, 0.0f, 0.0f);
+	}
+	
+	//float marchStep = (distanceFar - distanceNear) / kNumSamples;
+	float marchStep = distanceFar / kNumSamples;
+	
+	float mu = dot(ray.direction, gParameter.direction);
+	
+	float phaseR = RayleighPhaseFunction(mu);
+	float phaseM = HenyeyGreensteinPhaseFunction(mu);
+	
+	float opticalDepthR = 0.0f;
+	float opticalDepthM = 0.0f;
+	
+	float3 sumR = float3(0.0f, 0.0f, 0.0f);
+	float3 sumM = float3(0.0f, 0.0f, 0.0f);
+	
+	for (uint i = 0; i < kNumSamples; ++i) {
+		
+		float3 samplePoint = ray.origin + ray.direction * ((marchStep * i) + 0.5f * marchStep);
+		float height       = length(samplePoint) - kEarthRadius;
+		
+		float hr = exp(-height / kHr) * marchStep;
+		float hm = exp(-height / kHm) * marchStep;
+		
+		opticalDepthR += hr;
+		opticalDepthM += hm;
+		
+		Ray lightRay = (Ray)0;
+		lightRay.origin    = samplePoint;
+		lightRay.direction = gParameter.direction;
+		
+		float opticalDepthLightR = 0.0f;
+		float opticalDepthLightM = 0.0f;
+		
+		bool isOverground = GetSunLight(lightRay, atmosphere, opticalDepthLightR, opticalDepthLightM);
+		
+		if (isOverground) {
+			float3 tan
+				= kBetaR * (opticalDepthR + opticalDepthLightR) + kBetaM * (opticalDepthM + opticalDepthLightM) * 1.1f;
+
+			float3 attebuation = exp(-tan);
+			
+			sumR += attebuation * hr;
+			sumM += attebuation * hm;
+		}
+	}
+	
+	return gParameter.intensity * (phaseR * sumR * kBetaR + phaseM * sumM * kBetaM);
+}
+
 
 ////////////////////////////////////////////////////////////////////////////////////////////
 // common method
@@ -128,63 +220,22 @@ void main(uint3 dispatchThreadId : SV_DispatchThreadID) {
 		return; //!< already calculated
 	}
 
-	Ray rayD = GenerateRay((float2(index) + 0.5f) / gConfig.size);
-
-	float3 skyPos = IntersectionPos(rayD.origin, rayD.direction, gParameter.outerRadius);
-	if (skyPos.x == 0.0f) {
-		// カメラが大気の外
-		gMain[index] = float4(0.0f, 0.0f, 0.0f, 0.0f);
-		return;
-	}
-
-	float3 ray = skyPos - rayD.origin;
-	float far = length(skyPos - rayD.origin);
-	ray /= far;
-
+	Sphere atmosphere = (Sphere)0;
+	atmosphere.center = float3(0.0f, 0.0f, 0.0f);
+	atmosphere.radius = kAtmosphereRadius;
 	
-	float height      = length(rayD.origin);
-	float depth       = exp(kScaleOverScaleDepth * (gParameter.innerRadius - height));
-	float startAngle  = dot(ray, rayD.direction) / height;
-	float startOffset = depth * IntegralApproximation(startAngle);
-
-	const int kSamples = 5;
-
-	float sampleLength = far / float(kSamples);
-	float scaledLength = sampleLength * kScale;
-	float3 sampleRay   = ray * sampleLength;
-	float3 samplePoint = rayD.origin + sampleRay * 0.5f;
-
-	float3 frontColor = (float3)0;
-
-	for (int i = 0; i < kSamples; ++i) {
-		float fHeight      = length(samplePoint);
-		float fDepth       = exp(kScaleOverScaleDepth * (gParameter.innerRadius - fHeight));
-		
-		float fLightAngle = dot(gParameter.lightDir, samplePoint) / fHeight;
-		float fCameraAngle = dot(ray, samplePoint) / fHeight;
-
-		float fScatter = (startOffset + fDepth * (IntegralApproximation(fLightAngle) - IntegralApproximation(fCameraAngle)));
-
-		float3 attenuate = exp(-fScatter * (kInvWavelength * kKr4pi + kKm4pi));
-		frontColor      += attenuate * (fDepth * scaledLength);
-		samplePoint     += sampleRay;
-	}
-
-	float4 primaryColor   = float4(frontColor * (kInvWavelength * kKrESun), 1.0f);
-	float4 secondaryColor = float4(frontColor * kKmESun, 1.0f);
 	
-	float3 direction = normalize(rayD.origin - skyPos);
+	Ray ray = GenerateRay((float2(index) / gConfig.size) * 2.0f - 1.0f);
+	ray.origin.y += kEarthRadius;
+	
+	float3 col = GetIncidentLight(ray, atmosphere);
+	float d = dot(ray.direction, float3(0.0f, 1.0f, 0.0f));
+	
+	//if (d < 0.0f) {
+	//	col = lerp(col, float3(0.1f, 0.1f, 0.1f), abs(d));
 
-	float fCos = dot(gParameter.lightDir.xyz, direction) / length(direction);
-	float rayPhase = 0.75f * (1.0f + fCos * fCos);
-
-	float minPhase = 1.5f * ((1.0f - kG2) / (2.0f + kG2)) * (1.0f + fCos * fCos) / pow(abs(1.0f + kG2 - 2.0f * gParameter.g * fCos), 1.5f);
-
-	float3 raycolor = (primaryColor * rayPhase).rgb;
-	float3 miecolor = (secondaryColor * minPhase).rgb;
-
-	float3 color = float3(1.0f, 1.0f, 1.0f) - exp(-gParameter.exposure * (raycolor + miecolor));
-
-	gMain[index] = float4(color, 1.0f);
+	//}
+	
+	gMain[index] = float4(col, 1.0f);
 
 }
