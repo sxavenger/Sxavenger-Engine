d1a8c5bb41e877e186e4efaa528c3187a4813770
sxavenger <k023g0090@g.neec.ac.jp>
2025-04-23 14:07:49 +0900

    save

--- project/Packages/shaders/Render/Light/SkyLight.ps.hlsl
+++ project/Packages/shaders/Render/Light/SkyLight.ps.hlsl
@@ -61,13 +61,13 @@ Texture2D<float4> gBRDFLut : register(t0);
 
 float3 ApproximateBRDF(float3 diffuseAlbedo, float3 specularAlbedo, float3 n, float3 v, float3 r, float roughness) {
 
-	float NdotV = saturate(dot(n, v));
+	float NdotV = saturate(dot(n, v)) + kEpsilon;
 
-	const float2 brdf = gBRDFLut.Sample(gSampler, float2(NdotV, 1.0f - roughness)).rg;
+	const float2 brdf = gBRDFLut.SampleLevel(gSampler, float2(NdotV, 1.0f - roughness), 0).rg;
 
 	float lod = roughness * float(gSpecularParameter.miplevels);
 
-	float3 diffuseLight  = diffuseAlbedo * gDiffuseParameter.Sample(gSampler, r).rgb;
+	float3 diffuseLight  = diffuseAlbedo * gDiffuseParameter.Sample(gSampler, n).rgb;
 	float3 specularLight = specularAlbedo * gSpecularParameter.Sample(gSampler, r, lod).rgb;
 
 	float3 diffuse  = diffuseLight * diffuseAlbedo;
@@ -89,10 +89,12 @@ PSOutput main(PSInput input) {
 
 	//* Cameraの情報を取得
 	float3 v = normalize(gCamera.GetPosition() - surface.position);
-	float3 r = reflect(-v, surface.normal);
+	float3 r = -normalize(reflect(v, surface.normal));
 
 	// f0
 	static const float3 f0 = float3(0.04f, 0.04f, 0.04f); //!< 非金属の場合のf0
+
+	surface.albedo = float3(0.0f, 0.0f, 0.0f);
 
 	// diffuse Albedo
 	//!< 金属(metallic = 1.0f) -> 0.0f
@@ -104,7 +106,7 @@ PSOutput main(PSInput input) {
 	//!< 非金属(metallic = 0.0f) -> albedo
 	float3 specularAlbedo = lerp(f0, surface.albedo, surface.metallic);
 
-	output.color.rgb = ApproximateBRDF(diffuseAlbedo, specularAlbedo, surface.normal, v, r, surface.roughness);
+	output.color.rgb = ApproximateBRDF(diffuseAlbedo, specularAlbedo, surface.normal, v, r, surface.roughness) * 12;
 
 	output.color.a = 1.0f;
 	return output;
