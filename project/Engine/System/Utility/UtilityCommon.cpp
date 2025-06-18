92e0acd65a79f6b25cde00a0f113c47615498d4f
sxavenger <k023g0090@g.neec.ac.jp>
2025-06-18 12:18:33 +0900

    日時取得を実装

--- project/Engine/System/Utility/UtilityCommon.cpp
+++ project/Engine/System/Utility/UtilityCommon.cpp
@@ -6,6 +6,10 @@
 //* windows
 #include <comdef.h>
 
+//* c++
+#include <chrono>
+#include <format>
+
 ////////////////////////////////////////////////////////////////////////////////////////////
 // Convert string methods
 ////////////////////////////////////////////////////////////////////////////////////////////
@@ -37,5 +41,21 @@ std::wstring ToWString(const std::string& str) {
 	std::wstring result(sizeNeeded, 0);
 	MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), &result[0], sizeNeeded);
 	return result;
+
+}
+
+std::string GetCurrentDateTime(char separatorDate, char separatorDateTime, char separatorTime) {
+
+	auto now = std::chrono::zoned_time{ std::chrono::current_zone(), std::chrono::system_clock::now() };
+	auto local = now.get_local_time();
+
+	std::string fmt = std::format(
+		"%Y{}%m{}%d{}%H{}%M{}%S",
+		separatorDate, separatorDate,
+		separatorDateTime,
+		separatorTime, separatorTime
+	);
+
+	return std::vformat(fmt, std::make_format_args(local));
 
 }
