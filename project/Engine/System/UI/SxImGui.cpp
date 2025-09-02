38b613b416960a99d4907e65439efd247ccd1edb
sxavenger <k023g0090@g.neec.ac.jp>
2025-09-02 18:49:22 +0900

    a

--- project/Engine/System/UI/SxImGui.cpp
+++ project/Engine/System/UI/SxImGui.cpp
@@ -451,17 +451,15 @@ void SxImGui::HelpMarker(const char* label, const char* text, bool isSameline) {
 }
 
 void SxImGui::Image(ImTextureRef handle, const ImVec2& size) {
-	// タブ等を排除した全体のwindowSize計算
-	ImVec2 regionMax  = ImGui::GetWindowContentRegionMax();
-	ImVec2 regionMin  = ImGui::GetWindowContentRegionMin();
-	ImVec2 windowSize = { regionMax.x - regionMin.x, regionMax.y - regionMin.y };
+	
+	ImVec2 region = ImGui::GetContentRegionAvail();
 
 	// 画像アス比と分割したWindowアス比の計算
 	float textureAspectRatio = static_cast<float>(size.x) / static_cast<float>(size.y);
-	float windowAspectRatio  = windowSize.x / windowSize.y;
+	float windowAspectRatio  = region.x / region.y;
 
 	// 出力する画像サイズの設定
-	ImVec2 displayTextureSize = windowSize;
+	ImVec2 displayTextureSize = region;
 
 	// 画像サイズの調整
 	if (textureAspectRatio <= windowAspectRatio) {
