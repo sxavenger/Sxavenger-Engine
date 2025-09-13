53646d341c8149cbeadc3df55320806e377c81d2
sxavenger <k023g0090@g.neec.ac.jp>
2025-09-13 17:36:57 +0900

    visual studio version up

--- project/Engine/Render/GBuffer/FDeferredGBuffer.h
+++ project/Engine/Render/GBuffer/FDeferredGBuffer.h
@@ -25,14 +25,16 @@ public:
 		Albedo,
 		Normal,
 		MaterialARM,
-		Position //!< Hack: depthから復元できるように調整
+		Position, //!< Hack: depthから復元できるように調整
+		//Velocity,
 	};
 
 	//- Format
 	// Albedo:      [FMainGBuffer::ColorFormat]      float3 albedo, float NOT_USED
 	// Normal:      [DXGI_FORMAT_R10G10B10A2_UNORM]  float3 normal, float NOT_USED
-	// MaterialARM: [DXGI_FORMAT_R8G8B8A8_UNORM]     float ambient, float roughness, float metallic, float NOT_USED
+	// MaterialARM: [DXGI_FORMAT_R8G8B8A8_UNORM]     float ambient_occlusion, float roughness, float metallic, float NOT_USED
 	// Position:    [DXGI_FORMAT_R32G32B32A32_FLOAT] float3 position, float NOT_USED
+	// Velocity:    [DXGI_FORMAT_R16G16B16A16_FLOAT] float2 velocity, float2 NOT_USED
 
 public:
 
