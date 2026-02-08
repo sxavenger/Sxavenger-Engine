5f5c95476cd20248066454568234ee56219227f0
sxavenger <k023g0090@g.neec.ac.jp>
2026-02-08 13:52:10 +0900

    DxResourceの追加

---
+++ project/Engine/System/DirectX/DxObject/DxResource.cpp
@@ -0,0 +1,120 @@
+#include "DxResource.h"
+SXAVENGER_ENGINE_USING
+DXOBJECT_USING
+
+//-----------------------------------------------------------------------------------------
+// include
+//-----------------------------------------------------------------------------------------
+//* engine
+#include <Engine/System/Utility/StreamLogger.h>
+
+////////////////////////////////////////////////////////////////////////////////////////////
+// Resource class methods
+////////////////////////////////////////////////////////////////////////////////////////////
+
+void Resource::CreateCommitted(
+	DxObject::Device* device,
+	const D3D12_HEAP_PROPERTIES& prop, const D3D12_RESOURCE_DESC& desc, D3D12_RESOURCE_STATES state,
+	const std::optional<D3D12_CLEAR_VALUE>& clearValue) {
+
+	//!< resourceの作成
+	auto hr = device->GetDevice()->CreateCommittedResource(
+		&prop,
+		D3D12_HEAP_FLAG_NONE,
+		&desc,
+		state,
+		clearValue.has_value() ? &clearValue.value() : nullptr,
+		IID_PPV_ARGS(resource_.GetAddressOf())
+	);
+	DxObject::Assert(hr, L"resource create failed.");
+
+	//!< stateの保存
+	current_ = state;
+
+}
+
+void Resource::Reset() {
+	if (resource_ != nullptr) {
+		resource_.Reset();
+	}
+}
+
+void Resource::Transition(DxObject::CommandContext* context, D3D12_RESOURCE_STATES state) {
+	if (current_ == state) {
+		return;
+	}
+
+	context->TransitionResourceState(resource_.Get(), current_, state);
+	current_ = state;
+}
+
+void Resource::SetName(const std::wstring& name) const {
+	if (resource_ == nullptr) {
+		StreamLogger::EngineLog(L"[DXOBJECT Resource] warning | resource is null. cannot set name. name: " + name);
+		return; //!< resourceがnullptrの場合は設定できない.
+	}
+
+	resource_->SetName(name.c_str());
+}
+
+Resource Resource::CreateBuffer(
+	DxObject::Device* device,
+	D3D12_HEAP_TYPE type, size_t size, D3D12_RESOURCE_FLAGS flags, D3D12_RESOURCE_STATES state) {
+
+	DxObject::Resource resource = {};
+
+	//!< 制約
+	StreamLogger::AssertA(size != 0, "buffer size is zero.");
+
+	//!< propの設定
+	D3D12_HEAP_PROPERTIES prop = {};
+	prop.Type = type;
+
+	//!< descの設定
+	D3D12_RESOURCE_DESC desc = {};
+	desc.Dimension        = D3D12_RESOURCE_DIMENSION_BUFFER;
+	desc.Width            = size;
+	desc.Height           = 1;
+	desc.DepthOrArraySize = 1;
+	desc.MipLevels        = 1;
+	desc.SampleDesc.Count = 1;
+	desc.Layout           = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+	desc.Flags            = flags;
+
+	//!< resourceの生成
+	resource.CreateCommitted(device, prop, desc, state);
+
+	return resource;
+
+}
+
+Resource Resource::CreateTexture(
+	DxObject::Device* device,
+	D3D12_RESOURCE_DIMENSION dimension, const Vector3ui& size, UINT16 miplevels, DXGI_FORMAT format, D3D12_RESOURCE_FLAGS flags, D3D12_RESOURCE_STATES state,
+	const std::optional<D3D12_CLEAR_VALUE>& clearValue) {
+
+	DxObject::Resource resource = {};
+
+	//!< 制約
+	StreamLogger::AssertA(dimension != D3D12_RESOURCE_DIMENSION_BUFFER, "texture dimension is buffer.");
+
+	//!< propの設定
+	D3D12_HEAP_PROPERTIES prop = {};
+	prop.Type = D3D12_HEAP_TYPE_DEFAULT;
+
+	//!< descの設定
+	D3D12_RESOURCE_DESC desc = {};
+	desc.Dimension        = dimension;
+	desc.Width            = size.x;
+	desc.Height           = size.y;
+	desc.DepthOrArraySize = static_cast<UINT16>(size.z);
+	desc.MipLevels        = miplevels;
+	desc.Format           = format;
+	desc.SampleDesc.Count = 1;
+	desc.Flags            = flags;
+
+	//!< resourceの生成
+	resource.CreateCommitted(device, prop, desc, state, clearValue);
+	
+	return resource;
+}
---
+++ project/Engine/System/DirectX/DxObject/DxResource.h
@@ -0,0 +1,88 @@
+#pragma once
+
+//-----------------------------------------------------------------------------------------
+// include
+//-----------------------------------------------------------------------------------------
+//* DXOBJECT
+#include "DxObjectCommon.h"
+#include "DxDevice.h"
+#include "DxCommandContext.h"
+#include "DxDescriptor.h"
+
+//* c++
+#include <optional>
+
+////////////////////////////////////////////////////////////////////////////////////////////
+// DXOBJECT
+////////////////////////////////////////////////////////////////////////////////////////////
+DXOBJECT_NAMESPACE_BEGIN
+
+////////////////////////////////////////////////////////////////////////////////////////////
+// Resource class
+////////////////////////////////////////////////////////////////////////////////////////////
+class Resource {
+public:
+
+	//=========================================================================================
+	// public methods
+	//=========================================================================================
+
+	Resource()                    = default;
+	Resource(Resource&&) noexcept = default;
+
+	//* resource option *//
+
+	void CreateCommitted(
+		DxObject::Device* device,
+		const D3D12_HEAP_PROPERTIES& prop, const D3D12_RESOURCE_DESC& desc, D3D12_RESOURCE_STATES state,
+		const std::optional<D3D12_CLEAR_VALUE>& clearValue = std::nullopt
+	);
+
+	void Reset();
+
+	void Transition(
+		DxObject::CommandContext* context,
+		D3D12_RESOURCE_STATES state
+	);
+
+	void SetName(const std::wstring& name) const;
+
+	//* getter *//
+
+	ID3D12Resource* Get() const { return resource_.Get(); }
+
+	//* helper create methods *//
+
+	static Resource CreateBuffer(
+		DxObject::Device* device,
+		D3D12_HEAP_TYPE type, size_t size, D3D12_RESOURCE_FLAGS flags, D3D12_RESOURCE_STATES state
+	);
+
+	static Resource CreateTexture(
+		DxObject::Device* device,
+		D3D12_RESOURCE_DIMENSION dimension, const Vector3ui& size, UINT16 miplevels, DXGI_FORMAT format, D3D12_RESOURCE_FLAGS flags, D3D12_RESOURCE_STATES state,
+		const std::optional<D3D12_CLEAR_VALUE>& clearValue
+	);
+
+	//* delete [copy] *//
+	Resource(const Resource&)            = delete;
+	Resource& operator=(const Resource&) = delete;
+
+
+private:
+
+	//=========================================================================================
+	// private variables
+	//=========================================================================================
+
+	//* DirectX12 *//
+
+	ComPtr<ID3D12Resource> resource_;
+
+	//* state tracker *//
+
+	D3D12_RESOURCE_STATES current_ = D3D12_RESOURCE_STATE_COMMON;
+
+};
+
+DXOBJECT_NAMESPACE_END
