6b371c8f2edf0442095850a96b132c2eb19acd40
sxavenger <k023g0090@g.neec.ac.jp>
2026-03-06 17:05:55 +0900

    Demoの削除

--- project/main.cpp
+++ project/main.cpp
@@ -9,7 +9,6 @@
 #include <Engine/Execution/EngineExecution.h>
 #include <Engine/Execution/EditorExecution.h>
 #include <Engine/Execution/ExampleGameLoop.h>
-#include <Demo/Execution/DemoGameLoop.h>
 
 ////////////////////////////////////////////////////////////////////////////////////////////
 // main
@@ -24,7 +23,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 #endif
 
 	pipeline->Push<SxxEngine::ExampleGameLoop>();
-	//pipeline->Push<DemoGameLoop>();
 
 	pipeline->Run();
 
